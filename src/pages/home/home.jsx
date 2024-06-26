06ec8426dabdae1b7100b8f2d709f8f5f3015f6b
Francisco <figueroaf@moravian.edu>
2024-06-26 16:57:09 -0400

    custom cursor testing

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,9 +1,13 @@
+'use client'
+
 import React from 'react';
 import './home.css'
 import Navbar from '../../components/navbar/navbar';
 import { useState } from 'react';
 import { useEffect } from 'react';
 import styled, { keyframes } from 'styled-components';
+import { motion } from 'framer-motion';
+import useMousePosition from '../../animations/cursor/useMousePosition';
 
 const fadeIn = keyframes`
   from {
@@ -20,33 +24,90 @@ const AnimatedText = styled.h1`
 
 const Home = () => {
 
-    const texts = ['Photographer','Videographer','Film Editor']
+  /* Text Animation Start-------------------------------------------------------- */
+  const texts = ['Photographer', 'Videographer', 'Film Editor']
+  const [text, setText] = useState('Guaman')
 
+  useEffect(() => {
+    const intervalId = setInterval(() => {
+      setText((prevText) => {
+        const currentIndex = texts.indexOf(prevText);
+        const nextIndex = (currentIndex + 1) % texts.length;
+        return texts[nextIndex];
+      });
+    }, 5300); // Change text every 2 seconds
 
-    const [text,setText] = useState('Guaman')
+    return () => clearInterval(intervalId); // Clean up interval on component unmount
+  }, []);
 
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-          setText((prevText) => {
-            const currentIndex = texts.indexOf(prevText);
-            const nextIndex = (currentIndex + 1) % texts.length;
-            return texts[nextIndex];
-          });
-        }, 5300); // Change text every 2 seconds
+  /* Text Animation End-------------------------------------------------------- */
+
+  /* Cursor Animation Start-------------------------------------------------------- */
+
+  const [cursorVariant, setCursorVariant] = useState('default');
+  const [durationValue, setDurationValue] = useState(0);
+
+  const { x, y } = useMousePosition();
+
+  const variants = {
+    default: {
+      x: x - 14,
+      y: y - 14,
+      backgroundColor: 'white',
+    },
+
+    text: {
+      height: 150,
+      width: 150,
+      x: x - 70,
+      y: y - 70,
+      backgroundColor: 'black',
+
+    }
+
+  }
+
+  const textEnter = () => {
+    setCursorVariant('text');
+    setDurationValue(0.5);
+  }
+
+  const textLeave = () => {
+    setCursorVariant('default');
+    setDurationValue(0.5);
+    setTimeout(() => {
+      setDurationValue(0);  
+    }, 400);
+
+  }
     
-        return () => clearInterval(intervalId); // Clean up interval on component unmount
-      }, []);
 
 
-    return (
-        <div className='home'>
-            <Navbar />
-            <div className='title-div'>
-                <h1 className='firstName'>Brandon</h1>
-                <AnimatedText className='lastName' key={text}>{text}</AnimatedText>
-                </div>
-        </div>
-    )
+
+
+
+  return (
+    <div className='home'>
+      <Navbar />
+      <div className='title-div'>
+        <h1 onMouseEnter={textEnter} onMouseLeave={textLeave} className='firstName'>Brandon</h1>
+        <AnimatedText onMouseEnter={textEnter} onMouseLeave={textLeave} className='lastName' key={text}>{text}</AnimatedText>
+      </div>
+
+      < motion.div
+        className='cursor'
+        variants={variants}
+        animate={cursorVariant}
+        transition={{
+          type: 'tween',
+          ease: 'backOut',
+          duration: durationValue
+        }}
+      />
+
+
+    </div>
+  )
 }
 
 export default Home;
