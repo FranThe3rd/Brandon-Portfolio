885d218ef33b25790149cedf9ad05654aa50da18
Francisco <figueroaf@moravian.edu>
2024-06-29 15:06:32 -0400

    added some areas for react scroll

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import './navbar.css'
-import { useState,useContext} from 'react'
+import { useState, useContext } from 'react'
 import { NavLink } from 'react-router-dom'
 import { List } from 'phosphor-react'
-import {CursorContext} from '../cursor/CursorContext'
+import { CursorContext } from '../cursor/CursorContext'
 import { Link as LinkScroll } from 'react-scroll';
 
 
@@ -11,21 +11,21 @@ import { Link as LinkScroll } from 'react-scroll';
 const Navbar = () => {
 
     const [showNavbar, setShowNavbar] = useState(false)
-    const { cursorVariant, x, y, textEnter, textLeave,navTextEnter } = useContext(CursorContext);
+    const { cursorVariant, x, y, textEnter, textLeave, navTextEnter } = useContext(CursorContext);
 
 
 
     const handleShowNavbar = () => {
         setShowNavbar(!showNavbar)
     }
-  return (
-    <nav className="navbar">
+    return (
+        <nav className="navbar">
             <div className="container-nav">
                 <div className="logo">
-                <a href="https://www.instagram.com/filmic.brandon/" target='_blank'>
+                    <a href="https://www.instagram.com/filmic.brandon/" target='_blank'>
 
-                <div onMouseEnter={navTextEnter} onMouseLeave={textLeave} class="blob"></div>    
-                </a>                
+                        <div onMouseEnter={navTextEnter} onMouseLeave={textLeave} class="blob"></div>
+                    </a>
                 </div>
                 <div className="menu-icon" onClick={handleShowNavbar}>
                     <List size={40} color='white' />
@@ -33,38 +33,42 @@ const Navbar = () => {
                 <div className={`nav-elements  ${showNavbar && 'active'}`}>
                     <ul>
                         <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
+                        <LinkScroll smooth={true} duration={1000} to='home' offset={0}>
+
                             <NavLink onClick={handleShowNavbar} to="/">HOME</NavLink>
+                            </LinkScroll>
                         </li>
                         <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
-                        <LinkScroll smooth={true} duration={1000} to='card' offset={200}>
-
-                            <NavLink onClick={handleShowNavbar} >GALLERY</NavLink>
-                        </LinkScroll>
-                        </li>
-                        
-                        <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
-                        <NavLink onClick={handleShowNavbar} to="/libraries">ABOUT</NavLink>
-
+                            <LinkScroll smooth={true} duration={1000} to='card' offset={-100}>
+                                <NavLink onClick={handleShowNavbar} >GALLERY</NavLink>
+                            </LinkScroll>
                         </li>
 
                         <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
-                        <NavLink onClick={handleShowNavbar} to="/libraries">SERVICES</NavLink>
+                        <LinkScroll smooth={true} duration={3000} to='about-me' offset={-100}>
+                            <NavLink onClick={handleShowNavbar} >ABOUT</NavLink>
+                            </LinkScroll>
 
                         </li>
 
                         <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
-                        <NavLink onClick={handleShowNavbar} to="/libraries">PORTFOLIO</NavLink>
+                            <NavLink onClick={handleShowNavbar} to="/libraries">SERVICES</NavLink>
 
                         </li>
-                       
-                    
+
+                        <li onMouseEnter={navTextEnter} onMouseLeave={textLeave}>
+                            <NavLink onClick={handleShowNavbar} to="/libraries">PORTFOLIO</NavLink>
+
+                        </li>
+
+
                     </ul>
                 </div>
             </div>
         </nav>
 
- 
-  )
+
+    )
 }
 
 export default Navbar
