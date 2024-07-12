e87fed8fd5a5470921fdc2fa643796680293c317
Francisco <figueroaf@moravian.edu>
2024-07-12 17:27:35 -0400

    testing images

--- src/components/ZoomParallax/index.jsx
+++ src/components/ZoomParallax/index.jsx
@@ -8,6 +8,13 @@ import Picture4 from '../../assets/zoom/4.jpg';
 import Picture5 from '../../assets/zoom/5.jpg';
 import Picture6 from '../../assets/zoom/6.jpg';
 import Picture7 from '../../assets/zoom/7.jpg';
+import testing1 from '../../assets/testing/1.jpeg'
+import testing2 from '../../assets/testing/2.jpeg'
+import testing3 from '../../assets/testing/3.jpg'
+import testing4 from '../../assets/testing/4.jpg'
+import testing5 from '../../assets/testing/5.jpg'
+import testing6 from '../../assets/testing/6.jpg'
+import testing7 from '../../assets/testing/7.jpeg'
 
 
 const Index = () => {
@@ -33,10 +40,20 @@ const Index = () => {
     { src: Picture3, scale: scale9 },
   ];
 
+const testingPictures = [
+    { src: testing1, scale: scale4 },
+    { src: testing2, scale: scale5 },
+    { src: testing3, scale: scale6 },
+    { src: testing4, scale: scale5 },
+    { src: testing5, scale: scale6 },
+    { src: testing6, scale: scale8 },
+    { src: testing7, scale: scale9 },
+]
+
   return (
     <div ref={container} className={styles.container}>
       <div className={styles.sticky}>
-        {pictures.map(({ src, scale }, index) => (
+        {testingPictures.map(({ src, scale }, index) => (
           <motion.div key={index} style={{ scale }} className={styles.el}>
             <div className={styles.imageContainer}>
               <img
